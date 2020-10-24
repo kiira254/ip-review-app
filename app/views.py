209726e3ebbcb68a9a6b583a3a61c17628b806c6
kiira254 <nkamotho57@gmail.com>
2020-10-24 09:43:10 +0300

    add django registration signup in views

--- app/views.py
+++ app/views.py
@@ -1,6 +1,32 @@
-from django.shortcuts import render
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.shortcuts import render, redirect
+import datetime as dt
+from .models import Image, Profile, Like, Comments
+from .email import send_welcome_email
 from django.contrib.auth.decorators import login_required
+from .forms import LetterForm, ImageForm,SignupForm,ProfileForm, CommentForm
+from django.contrib.auth.models import User
 
 # Create your views here.
+def signup(request):
+    name = "Sign Up"
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            email = form.cleaned_data.get('email')
+            name = form.cleaned_data.get('username')
+            send_mail(
+            'Welcome to Mini-Insta.',
+            f'Hello {name},\n '
+            'Welcome to Mini-Insta.',
+            'nkamotho69@gmail.com',
+            [email],
+            fail_silently=False,
+            )
+        return redirect('post')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/registration_form.html', {'form': form, 'name':name})
 
 @login_required(login_url='/accounts/login/')
--- review/urls.py
+++ review/urls.py
@@ -18,9 +18,15 @@ from django.contrib import admin
 from django.urls import path,include
 from django.contrib.auth import logout
 from django.contrib import admin
+from django_registration.backends.one_step.views import RegistrationView
+
 # from tinymce import urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/register/',
+        RegistrationView.as_view(success_url='/profile/'),
+        name='django_registration_register'),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
 ]
