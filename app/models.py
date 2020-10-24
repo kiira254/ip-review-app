61d56bbeaa22a2264fbc15343d258ca362874f13
kiira254 <nkamotho57@gmail.com>
2020-10-24 12:44:44 +0300

    add profile model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.dispatch import receiver
+
 
 # Create your models here.
+class Profile(models.Model):
+  Profile_photo = models.ImageField(upload_to = 'photos/')
+  bio = models.CharField(max_length=200)
+  contact = models.EmailField()
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+  @receiver(post_save, sender=User)
+  def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+      Profile.objects.create(user=instance)
+
+  @receiver(post_save, sender=User)
+  def save_user_profile(sender, instance, **kwargs):
+      instance.profile.save()
+
+  post_save.connect(save_user_profile, sender=User)
+
+  def save_profile(self):
+    self.save()
+
+  def delete_profile(self):
+    self.delete()
