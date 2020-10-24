402c829691f1398d72a22ce47b4f96b779653d4f
kiira254 <nkamotho57@gmail.com>
2020-10-24 08:02:05 +0300

    add registration.html

--- review/settings.py
+++ review/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app.apps.ApConfig',
+    'app.apps.AppConfig',
     'bootstrap4',
 ]
 
