f1866ab72fabc9ea8b2d0699193bd12678d8141a
kiira254 <nkamotho57@gmail.com>
2020-10-26 06:42:18 +0300

    add test

--- app/tests.py
+++ app/tests.py
@@ -6,52 +6,41 @@ from .models import Profile, Project, Rating
 
 
 class ProfileTestClass(TestCase):
-  """  
-  Test class that tests profile
-  """
+
   def setUp(self):
-    self.prof =Profile(profpic='test.jpg', bio='test bio', contact='cchelangat16@gmail.com',user=1)
+    self.profile =Profile(profile_photo='test.jpg', bio='test bio', contact='nkamotho69@gmail.com',user_id=1)
 
   def test_instance(self):
-      self.assertTrue(isinstance(self.prof, Profile))
+      self.assertTrue(isinstance(self.profile, Profile))
 
   def test_save_method(self):
-      """
-      Function to test that profile is being saved
-      """
-      self.prof.save_profile()
+     
+      self.profile.save_profile()
       profiles = Profile.objects.all()
       self.assertTrue(len(profiles) > 0)
 
   def test_delete_method(self):
-      """
-      Function to test that a profile can be deleted
-      """
-      self.prof.save_profile()
-      self.prof.delete_profile()
+      
+      self.profile.save_profile()
+      self.profile.delete_profile()
       profiles = Profile.objects.all()
       self.assertTrue(len(profiles) == 0)
 
 class ProjecTestClass(TestCase):
-  """  
-  Tests Project class and its functions
-  """
-
+  def setUp(self):
+    self.project =Project(title='test',image='test.jpg', description='test description', link='htt//.test')
+  
   def test_instance(self):
       self.assertTrue(isinstance(self.project, Project))
 
   def test_save_method(self):
-      """
-      Testing whether a new project is being saved
-      """
+      
       self.project.save_project()
       projects = Project.objects.all()
       self.assertTrue(len(projects) > 0)
 
   def test_delete_method(self):
-      """
-      Function to test that a project can be deleted
-      """
+      
       self.project.save_project()
       self.project.delete_project()
       projects = Project.objects.all()
